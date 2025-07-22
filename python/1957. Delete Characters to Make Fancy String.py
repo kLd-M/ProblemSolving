ac3b2aea14a70479690d39e944696e66b716a9ec
kLd-M <kld.madi88@gmail.com>
2025-07-22 05:58:39 +0300

    solved todays  problem

---
+++ python/1957. Delete Characters to Make Fancy String.py
@@ -0,0 +1,13 @@
+
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+
+        if len(s) < 3:
+            return s
+        result = []
+        for char in s:
+            if len(result) >= 2 and result[-1] == char and result[-2] == char:
+                continue
+            result.append(char)
+
+        return ''.join(result)
